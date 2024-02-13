9c3cd874cfa974f30297638abe6ae6f6cb25669d
jacksonhalesc@gmail.com <ForTheRepublicAndDemocracy@gmail.com>
2024-02-13 06:15:50 -0500

    dtsdfdsfds

--- assignments/assignment8/script.js
+++ assignments/assignment8/script.js
@@ -3,35 +3,46 @@ function changePicture() {
     var image = document.getElementById('yogaImage');
 
     if (input !== '') {
-      switch(input[input.length - 1]) {
-        case 'b':
-          image.src = 'read.jpg';
-          break;
-        case 'c':
-          image.src = 'clown.jpg';
-          break;
-        case 'p':
-          image.src = 'birthday.jpg';
-          break;
-        case 'r':
-          image.src = 'rain.jpg';
-          break;
-        case 's':
-          image.src = 'shovel.jpg';
-          break;
-        case 'w':
-          image.src = 'work.jpg';
-          break;
-        default:
-          // Handle default case, e.g., show a default image
-          break;
-      }
+        switch(input[input.length - 1]) {
+            case 'b':
+                image.src = 'read.jpg';
+                break;
+            case 'c':
+                image.src = 'clown.jpg';
+                break;
+            case 'p':
+                image.src = 'birthday.jpg';
+                break;
+            case 'r':
+                image.src = 'rain.jpg';
+                break;
+            case 's':
+                image.src = 'shovel.jpg';
+                break;
+            case 'w':
+                image.src = 'work.jpg';
+                break;
+            default:
+                // Handle default case, e.g., show a default image
+                break;
+        }
+        image.style.display = 'block';
     }
-  }
+}
 
-  function changeYogaImage() {
+function changeYogaImage() {
     var slider = document.getElementById('yogaSlider');
     var image = document.getElementById('yogaImage');
     var imageNumber = parseInt(slider.value);
     image.src = 'yoga' + imageNumber + '.jpg';
-  }
+}
+
+function showExercise1() {
+    document.getElementById('exercise1').style.display = 'block';
+    document.getElementById('exercise2').style.display = 'none';
+}
+
+function showExercise2() {
+    document.getElementById('exercise1').style.display = 'none';
+    document.getElementById('exercise2').style.display = 'block';
+}
