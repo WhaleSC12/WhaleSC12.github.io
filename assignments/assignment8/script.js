62983e88a07021a911315dc02d56e8d3baa757c4
jacksonhalesc@gmail.com <ForTheRepublicAndDemocracy@gmail.com>
2024-02-13 06:08:31 -0500

    dtdsdfdsfdsf

--- assignments/assignment8/script.js
+++ assignments/assignment8/script.js
@@ -1,29 +1,7 @@
-//  menu
-function toggleMenu() {
-    var menuItems = document.getElementById('menuItems');
-    menuItems.style.display = (menuItems.style.display === 'block') ? 'none' : 'block';
-    var arrow = document.getElementById('toggleMenu');
-    arrow.innerHTML = (menuItems.style.display === 'block') ? '&#9650;' : '&#9660;';
-  }
-  
-  //  toggling 
-  function toggleExercise(exercise) {
-    var enterCommand = document.getElementById('enterCommand');
-    var yogaSlide = document.getElementById('yogaSlide');
-    if (exercise === 1) {
-      enterCommand.style.display = 'block';
-      yogaSlide.style.display = 'none';
-    } else if (exercise === 2) {
-      enterCommand.style.display = 'none';
-      yogaSlide.style.display = 'block';
-    }
-  }
-  
-  // Exercise 1
-  function changePicture() {
+function changePicture() {
     var input = document.getElementById('commandInput').value.trim().toLowerCase();
     var image = document.getElementById('yogaImage');
-  
+
     if (input !== '') {
       switch(input[input.length - 1]) {
         case 'b':
@@ -50,13 +28,10 @@ function toggleMenu() {
       }
     }
   }
-  
-  
-  //  Exercise 2
+
   function changeYogaImage() {
     var slider = document.getElementById('yogaSlider');
     var image = document.getElementById('yogaImage');
     var imageNumber = parseInt(slider.value);
     image.src = 'yoga' + imageNumber + '.jpg';
   }
-  
