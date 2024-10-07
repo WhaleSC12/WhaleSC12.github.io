3792adbd84f3912306778eb96cf2100413b4f4b5
WhaleSC12 <WJHALE@email.sc.edu>
2024-10-07 03:04:58 -0400

    robinfdsdddddddd

--- projects/part5/script.js
+++ projects/part5/script.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     const navLinks = document.getElementById("navLinks");
     navLinks.classList.toggle("active");
 }
+
+function toggleMenu() {
+    document.getElementById("mobileNav").style.width = "250px";
+}
+
+function closeMenu() {
+    document.getElementById("mobileNav").style.width = "0";
+}
